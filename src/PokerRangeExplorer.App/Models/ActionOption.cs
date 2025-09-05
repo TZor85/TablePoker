e8adeb9ee43e7160f9e355a22d0ff55b4680fac1
Alberto <alberto.cervantes@smsdata.es>
2025-09-05 12:52:11 +0200

    delete

--- src/PokerRangeExplorer.App/Models/ActionOption.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PokerRangeExplorer.App.Models;
-
-public class ActionOption
-{
-    public string Action { get; set; } = string.Empty;
-    public int Percentage { get; set; }
-}
--- src/PokerRangeExplorer.App/Models/PokerRange.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PokerRangeExplorer.App.Models;
-
-public class PokerRange
-{
-    public string Name { get; set; } = string.Empty;
-    public List<Position> Positions { get; set; } = new();
-}
